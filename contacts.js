8bcc91a7d1bd7bde162a5405faae03cbb4f1b3b6
Dan Retegan <danretegan@yahoo.com>
2024-04-27 10:42:13 +0300

    add Create
    function addContact(name, email, phone)

--- contacts.js
+++ contacts.js
@@ -1,7 +1,8 @@
 import colors from "colors";
 import { dirname } from "path";
+import { randomUUID } from "crypto";
 import { fileURLToPath } from "url";
-import { readFile } from "node:fs/promises";
+import { readFile, writeFile } from "fs/promises";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const contactsPath = `${__dirname}\\db\\contacts.json`;
@@ -11,13 +12,40 @@ const contactsPath = `${__dirname}\\db\\contacts.json`;
 //! Read:
 async function listContacts() {
   try {
-    const contents = await readFile(contactsPath, { encoding: "utf8" });
-    const products = JSON.parse(contents);
-    console.table(products);
+    const rawData = await readFile(contactsPath, { encoding: "utf8" });
+    const contacts = JSON.parse(rawData);
+    console.table(contacts);
   } catch (err) {
     console.log("There is an error:".bgRed);
     console.error(err);
   }
 }
 
-listContacts();
+//! Create:
+async function addContact(name, email, phone) {
+  try {
+    const rawData = await readFile(contactsPath, { encoding: "utf8" });
+    const contacts = JSON.parse(rawData);
+
+    if (!name || !email || !phone) {
+      throw new Error("Invalid data provided!");
+    }
+
+    const newContactId = randomUUID();
+    const newContact = {
+      id: newContactId,
+      name: name,
+      email: email,
+      phone: phone,
+    };
+
+    contacts.push(newContact);
+    const parsedContacts = JSON.stringify(contacts);
+    await writeFile(contactsPath, parsedContacts);
+
+    console.log("The contact has been created successfully!".bgGreen);
+  } catch (err) {
+    console.log("There is an error:".bgRed);
+    console.error(err);
+  }
+}
