26b6d78cfa88e142e627009a445863784779f7bf
Dan Retegan <danretegan@yahoo.com>
2024-04-27 20:26:13 +0300

    yargs
    # Obținerea și afișarea întregii liste de contacte sub formă de tabel (console.table):
        node index.js list
    
        # Obținerea unui contact după id:
        node index.js get idxyz3
    
        # Adăugarea unui contact:
        node index.js add Mango mango@gmail.com 322-22-22
    
        # Ștergerea unui contact:
        node index.js remove idxyz3

--- index.js
+++ index.js
@@ -1,6 +1,78 @@
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
 import {
   listContacts,
   addContact,
   removeContact,
   getContactById,
 } from "./contacts.js";
+
+yargs(hideBin(process.argv))
+  .command({
+    command: "list",
+    describe: "List all contacts",
+    handler: () => invokeAction({ action: "list" }),
+  })
+  .command({
+    command: "get <id>",
+    describe: "Get contact by ID",
+    builder: (yargs) => yargs.positional("id", { describe: "Contact ID" }),
+    handler: (argv) => invokeAction({ action: "get", id: argv.id }),
+  })
+  .command({
+    command: "add <name> <email> <phone>",
+    describe: "Add a new contact",
+    builder: (yargs) => {
+      yargs.positional("name", { describe: "Contact name" });
+      yargs.positional("email", { describe: "Contact email" });
+      yargs.positional("phone", { describe: "Contact phone number" });
+    },
+    handler: (argv) =>
+      invokeAction({
+        action: "add",
+        name: argv.name,
+        email: argv.email,
+        phone: argv.phone,
+      }),
+  })
+  .command({
+    command: "remove <id>",
+    describe: "Remove contact by ID",
+    builder: (yargs) => yargs.positional("id", { describe: "Contact ID" }),
+    handler: (argv) => invokeAction({ action: "remove", id: argv.id }),
+  }).help(`
+    # Obținerea și afișarea întregii liste de contacte sub formă de tabel (console.table):
+    node index.js list
+
+    # Obținerea unui contact după id:
+    node index.js get idxyz3
+
+    # Adăugarea unui contact:
+    node index.js add Mango mango@gmail.com 322-22-22
+
+    # Ștergerea unui contact:
+    node index.js remove idxyz3
+  `).argv;
+
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      listContacts();
+      break;
+
+    case "get":
+      getContactById(id);
+      break;
+
+    case "add":
+      addContact(name, email, phone);
+      break;
+
+    case "remove":
+      removeContact(id);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
